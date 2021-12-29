85839be09b4afef3c0c84701b2f9f1b26d74a346
Yarizzer <yarizzer@gmail.com>
2021-12-29 16:54:18 +0300

    6. Идеальный вес [#41584 #245171] fixed

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,14 +4,11 @@ import static java.lang.Math.sqrt;
 
 public class Point {
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
+        double result = distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
     }
 
     private static double distance(int x1, int y1, int x2, int y2) {
-        double first = Math.pow((x2 - x1), 2);
-        double second = Math.pow((y2 - y1), 2);
-
-        return sqrt(first + second);
+        return sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 }
